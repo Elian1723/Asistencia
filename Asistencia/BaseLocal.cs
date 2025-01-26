dbbde30c041441d0812140c42c1102e00436d787
Elián Barrios <elianbarrios1707@outlook.com>
2025-01-25 21:25:49 -0600

    first commit

---
+++ Asistencia/BaseLocal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace Asistencia
+{
+    public static class BaseLocal
+    {
+        public static string Cadena { get; } = $@"Data Source=C:\Asistencia\asistencia.sqlite;Version=3;";
+
+        public static bool Comprobar()
+        {
+            try
+            {
+                if (!Directory.Exists(@"C:\Asistencia"))
+                {
+                    Directory.CreateDirectory(@"C:\Asistencia");
+                }
+
+                if (!File.Exists(@"C:\Asistencia\asistencia.sqlite"))
+                {
+                    SQLiteConnection.CreateFile(@"C:\Asistencia\asistencia.sqlite");
+                }
+
+                using (var connection = new SQLiteConnection(Cadena))
+                {
+                    connection.Open();
+
+                    string query = @"
+                            CREATE TABLE IF NOT EXISTS Alumno(
+	                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+	                            Nombre TEXT NOT NULL
+                            );";
+
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Asistencia/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Asistencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridViewAlumnos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.buttonGenerar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewAlumnos
+            // 
+            this.dataGridViewAlumnos.AllowUserToAddRows = false;
+            this.dataGridViewAlumnos.AllowUserToDeleteRows = false;
+            this.dataGridViewAlumnos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewAlumnos.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewAlumnos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridViewAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAlumnos.Location = new System.Drawing.Point(13, 56);
+            this.dataGridViewAlumnos.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewAlumnos.Name = "dataGridViewAlumnos";
+            this.dataGridViewAlumnos.Size = new System.Drawing.Size(743, 377);
+            this.dataGridViewAlumnos.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre del alumno:";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxNombre.Location = new System.Drawing.Point(184, 18);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(431, 26);
+            this.textBoxNombre.TabIndex = 0;
+            // 
+            // buttonGenerar
+            // 
+            this.buttonGenerar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonGenerar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerar.Location = new System.Drawing.Point(637, 12);
+            this.buttonGenerar.Name = "buttonGenerar";
+            this.buttonGenerar.Size = new System.Drawing.Size(119, 37);
+            this.buttonGenerar.TabIndex = 1;
+            this.buttonGenerar.Text = "Generar";
+            this.buttonGenerar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 446);
+            this.Controls.Add(this.buttonGenerar);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewAlumnos);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(785, 485);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Asistencia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewAlumnos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Button buttonGenerar;
+    }
+}
+
